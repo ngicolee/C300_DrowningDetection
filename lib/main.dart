057797b4ab6a3f9e229e39e39db81530045ee414
Dexter Chua <dchua647@gmail.com>
2022-06-02 22:08:51 +0800

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // Home of MaterialApp
+      home: SplashPage(duration: 3, goToPage: const WelcomePage())));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// Splash Page StatelessWidget
+// ignore: must_be_immutable
+class SplashPage extends StatelessWidget {
+  int duration = 0;
+  Widget goToPage;
 
-  // This widget is the root of your application.
+  SplashPage({Key? key, required this.goToPage, required this.duration})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    Future.delayed(Duration(seconds: duration), () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => goToPage));
     });
+    return Scaffold(
+        body: Container(
+            color: const Color.fromARGB(255, 0, 183, 255),
+            alignment: Alignment.center,
+            child:
+                const Icon(Icons.pool_sharp, color: Colors.white, size: 200)));
   }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        body: Container(
+      color: Colors.black,
+      child: Stack(children: [
+        Positioned.fill(
+          child: Opacity(
+            opacity: 0.3,
+            child: Image.asset('assets/imgs/Background.jpg', fit: BoxFit.cover),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ClipOval(
+                  child: Container(
+                      width: 180,
+                      height: 180,
+                      color: const Color.fromARGB(255, 0, 183, 255),
+                      alignment: Alignment.center,
+                      child: const Icon(Icons.pool_sharp,
+                          color: Colors.white, size: 130)),
+                ),
+                const SizedBox(height: 50),
+                const Text('Drowning Detection',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold)),
+                const SizedBox(height: 30),
+                const Text(
+                    'C300 FYP Project \n Members: Nicole, Nicolette, Yuan Wei, Dexter',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 15)),
+                const SizedBox(height: 40),
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color.fromARGB(255, 0, 183, 255),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 32, vertical: 4),
+                        minimumSize: const Size(350, 20)),
+                    icon: const Icon(Icons.login_outlined,
+                        size: 32, color: Colors.white),
+                    label: buildText('Login to System'),
+                    onPressed: () {}),
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        primary: const Color.fromARGB(255, 0, 183, 255),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 32, vertical: 4),
+                        minimumSize: const Size(350, 20)),
+                    icon: const Icon(Icons.app_registration_outlined,
+                        size: 32, color: Colors.white),
+                    label: buildText('Registration'),
+                    onPressed: () {})
+              ]),
+        )
+      ]),
+    ));
   }
 }
+
+// Button Test Builder
+Text buildText(String text) => Text(text, style: const TextStyle(fontSize: 28));
