fafe82cdfab750a022c318eaf50915ea0cd7bcfa
Dexter Chua <dchua647@gmail.com>
2022-06-04 17:06:17 +0800

    Third Commit: Added HomePage

---
+++ lib/helpers/iconhelper.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+class IconFontHelper {
+  static const String QR_CODE = 'a';
+  static const String MORE_DOTS = 'b';
+  static const String DETECTION_SYSTEM = 'c';
+  static const String APP_ICON = 'd';
+}
---
+++ lib/widgets/categoryicons.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: unnecessary_this, must_be_immutable
+
+import 'package:c300drowningdetection/widgets/iconfont.dart';
+import 'package:flutter/material.dart';
+
+class CategoryIcon extends StatelessWidget {
+  Color color;
+  String iconName;
+  double size;
+
+  CategoryIcon(
+      {Key? key, required this.color, required this.iconName, this.size = 30})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+        child: Container(
+            color: this.color,
+            padding: const EdgeInsets.all(10),
+            child: IconFont(
+                color: Colors.white, iconName: this.iconName, size: this.size)));
+  }
+}
